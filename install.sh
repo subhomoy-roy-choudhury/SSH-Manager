ca6bfffc6bfc6f786f331f51aa5ade34c86a5699
thor-ai <thorai@pop-os.localdomain>
2022-02-21 11:14:36 +0530

    Initial script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash  
+ 
+# value=$(tr '\n' ' ' < apt-dependencies.txt)
+# echo "$value"  
+# echo "Start Installation"
+# # echo "sudo apt-get install $value"
+# sudo apt-get -y install $value
+
+sudo apt-get -y update
+
+echo "Installing yq"
+
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+sudo chmod a+x /usr/local/bin/yq
+yq --version
+
+# Creating SSH Folder in Documents Directory
+
+login_user=$(whoami);
+doc_path="/home/${login_user}/Documents/SSH-Manager";
+mkdir $doc_path;
+mkdir "${doc_path}/pem-list"
+mkdir "${doc_path}/ssh-list"
+
+echo '${title}:
+  username : ${username}
+  ipaddr : ${ipaddr}
+  ssh_key : ${ssh_key}
+' > ${doc_path}/template.yml
